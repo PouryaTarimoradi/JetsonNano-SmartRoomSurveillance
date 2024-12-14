00e5cd797ba22790b08902b1dcece8a13b0d679d
TestToAI <153095899+PouryaTarimoradi@users.noreply.github.com>
2024-12-14 15:49:05 +0100

    Update bot_config.py

--- bot_config/bot_config.py
+++ bot_config/bot_config.py
@@ -1,1 +1,10 @@
+# bot_config.py
 
+import telegram
+
+# Set up your bot token and chat ID
+bot_token = "////////"  # Replace with your bot token
+chat_id = '///////'  # Replace with your chat ID
+
+# Create the bot instance
+bot = telegram.Bot(token=bot_token)
